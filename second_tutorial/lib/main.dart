6dd346a3a14b8367c21e2890a480e29ed185759f
Calebfalz <faleyecassiel005@gmail.com>
2021-04-28 23:26:36 +0100

    alignment, gradient, boxshadow, borderRadius, clipBehaviour, decoration,textAlign

--- second_tutorial/lib/main.dart
+++ second_tutorial/lib/main.dart
@@ -19,8 +19,40 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text('Hi Flutter'),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          clipBehavior:
+              Clip.antiAlias, // the content will not be outside the container
+          width: 100.0,
+          height: 100.0,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.red,
+                  Colors.indigo,
+                ],
+              ),
+              color: Colors.red,
+              //shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+                ),
+              ]),
+          alignment: Alignment.center, //where you want to place the child
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
